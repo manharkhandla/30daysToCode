5a5fbb8f185a1acc9d050aebab9f8730a3108e7d
manharkhandla <khandlamanhar8038@gmail.com>
2025-07-29 22:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (34.58%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-
-   void getSum(string num,int k,int &ans1)
-    {
-        if(k==0) return;
-        //cout<<num <<" " << ans <<" " << k << " " <<endl;
-        int n = num.size();
-        int ans = 0;
-        for(int i=0;i<n;i++)
-        {
-            ans += num[i]-'0';
-        }
-        ans1 = ans;
-
-        num = to_string(ans);
-        //cout<<num <<" " << ans <<" " << k << " " <<endl;
-        getSum(num,k-1,ans1);
-
-        return;
-    }
-
     int getLucky(string s, int k) {
         string num = "";
         int n = s.size();
-        for(int i=0;i<n;i++)
-        {
+
+        for (int i=0;i<n;i++) {
             num += to_string(s[i] - 'a' + 1);
         }
 
-        cout<<num<<endl;
-        int ans = 0;
-        int ans1 = ans;
-        getSum(num,k,ans1);
+        
+        while (k--) {
+            int sum = 0;
+            int d = num.size();
+            for (int i=0;i<d;i++) {
+                sum += num[i] - '0';
+            }
+            num = to_string(sum);
+        }
 
-        return ans1;
+        return stoi(num);
     }
-
 };
