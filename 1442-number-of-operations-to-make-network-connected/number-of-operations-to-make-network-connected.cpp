84950d7b8510b161ce4e532336ec8a10f19a3409
manharkhandla <khandlamanhar8038@gmail.com>
2025-08-14 20:30:07 +0530

    Time: 2 ms (89.23%) | Memory: 42.1 MB (92.18%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,64 @@
+
+
+
+class Solution {
+public:
+
+
+
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int i)
+    {
+        if(i==parent[i]) return i;
+
+        return parent[i] = find(parent[i]);
+    }
+
+    void Union(int x,int y)
+    {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent) return;
+
+
+        if(rank[x_parent] > rank[y_parent])
+        {
+            parent[y_parent] = x_parent;
+        }
+
+        else if(rank[x_parent] < rank[y_parent])
+        {
+            parent[x_parent] = y_parent;
+        }
+        
+        else 
+        {
+            parent[y_parent] = x_parent; // attach y to x (or vice versa)
+            rank[x_parent]++;
+        }
+        
+    }
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+    if (n - 1 > connections.size()) return -1;
+
+    parent.resize(n);
+    rank.resize(n);
+    for (int i = 0; i < n; i++) parent[i] = i;
+
+    for (auto& c : connections) {
+        Union(c[0], c[1]);
+    }
+
+    int components = 0;
+    for (int i = 0; i < n; i++) {
+        if (find(i) == i) components++;
+    }
+
+    return components - 1; // operations needed
+}
+
+};
