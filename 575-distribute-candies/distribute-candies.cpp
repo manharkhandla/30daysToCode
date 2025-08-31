1a4a55b88aef7021c1e29993b89c816fffe789a6
manharkhandla <khandlamanhar8038@gmail.com>
2025-08-31 15:23:08 +0530

    Time: 49 ms (86.59%) | Memory: 85.4 MB (99.44%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         int n = candyType.size();
-        // int ans = 0;
-        // int lastEatCandy = -10001;
-        // int totalEatedCandy = 0;
+        int ans = 0;
+        int lastEatCandy = -10001;
+        int totalEatedCandy = 0;
 
-        // sort(candyType.begin(),candyType.end());
+        sort(candyType.begin(),candyType.end());
 
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(totalEatedCandy >= n/2) return ans;
-        //     if(candyType[i]==lastEatCandy) continue;
-        //     else
-        //     {
-        //         ans++;
-        //         totalEatedCandy++;
-        //         lastEatCandy = candyType[i];
-        //     }
-        // }
-
-        // return ans;
-
-        unordered_set<int> uniqueCandies;
-        
-        for (int i=0;i<n;i++) {
-            uniqueCandies.insert(candyType[i]);
+        for(int i=0;i<n;i++)
+        {
+            if(totalEatedCandy >= n/2) return ans;
+            if(candyType[i]==lastEatCandy) continue;
+            else
+            {
+                ans++;
+                totalEatedCandy++;
+                lastEatCandy = candyType[i];
+            }
         }
 
-        int uniqueCount = uniqueCandies.size();
+        return ans;
+
+        // unordered_set<int> uniqueCandies;
         
-        return min(uniqueCount, n / 2);
+        // for (int i=0;i<n;i++) {
+        //     uniqueCandies.insert(candyType[i]);
+        // }
+
+        // int uniqueCount = uniqueCandies.size();
+        
+        // return min(uniqueCount, n / 2);
 
     }
 };
