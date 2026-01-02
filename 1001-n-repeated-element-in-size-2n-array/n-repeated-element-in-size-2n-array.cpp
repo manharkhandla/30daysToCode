20f974adc809082e456fe79a6bfd877052804469
manharkhandla <khandlamanhar8038@gmail.com>
2026-01-02 22:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (53.60%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        
+        unordered_map<int, int> freq;
+        for (int x : nums) 
+        {
+            freq[x]++;
+            if (freq[x] > 1) return x;
+        }
+        
+        return -1;
+    
+    }
+};
