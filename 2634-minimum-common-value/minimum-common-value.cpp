470f430b47696946b49682cc7b9c3583cdcb9676
manharkhandla <khandlamanhar8038@gmail.com>
2026-05-19 16:47:11 +0530

    Time: 3 ms (38.91%) | Memory: 54.6 MB (58.81%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+
+        int ptr1 = 0;
+        int ptr2 = 0;
+
+        while(ptr1<n && ptr2<m)
+        {
+            if(nums1[ptr1]==nums2[ptr2]) return nums1[ptr1];
+            if(nums1[ptr1]<nums2[ptr2]) ptr1++;
+            else ptr2++;
+        }
+
+        return -1;
+    }
+};
